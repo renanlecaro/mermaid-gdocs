42be3f856a14ea2cfa9b37828f23b6ef5b071a45
Renan LE CARO <renan.lecaro@gmail.com>
2019-06-11 14:39:09 +0200

    Updated website to link to github, removed samples (theyll be menu items)

--- src/index.js
+++ src/index.js
@@ -64,62 +64,62 @@ byId('cancel').addEventListener('click',e=>{
   cancelEdit()
 })
 
-byId('Flowchart').addEventListener('click',e=>{
-  e.preventDefault()
-  setCode(`
-graph LR
-    A[Hard edge] -->|Link text| B(Round edge)
-    B --> C{Decision}
-    C -->|One| D[Result one]
-    C -->|Two| E[Result two]
-`)
-})
+// byId('Flowchart').addEventListener('click',e=>{
+//   e.preventDefault()
+//   setCode(`
+// graph LR
+//     A[Hard edge] -->|Link text| B(Round edge)
+//     B --> C{Decision}
+//     C -->|One| D[Result one]
+//     C -->|Two| E[Result two]
+// `)
+// })
 
-byId('Sequence').addEventListener('click',e=>{
-  e.preventDefault()
-  setCode(`sequenceDiagram
-    Alice->>Bob: Hello Bob, how are you?
-    alt is sick
-        Bob->>Alice: Not so good :(
-    else is well
-        Bob->>Alice: Feeling fresh like a daisy
-    end
-    opt Extra response
-        Bob->>Alice: Thanks for asking
-    end
-`)
-})
+// byId('Sequence').addEventListener('click',e=>{
+//   e.preventDefault()
+//   setCode(`sequenceDiagram
+//     Alice->>Bob: Hello Bob, how are you?
+//     alt is sick
+//         Bob->>Alice: Not so good :(
+//     else is well
+//         Bob->>Alice: Feeling fresh like a daisy
+//     end
+//     opt Extra response
+//         Bob->>Alice: Thanks for asking
+//     end
+// `)
+// })
 
-byId('Gantt').addEventListener('click',e=>{
-  e.preventDefault()
-  setCode(`gantt
-   dateFormat  YYYY-MM-DD
-   title Adding GANTT diagram functionality to mermaid
+// byId('Gantt').addEventListener('click',e=>{
+//   e.preventDefault()
+//   setCode(`gantt
+//    dateFormat  YYYY-MM-DD
+//    title Adding GANTT diagram functionality to mermaid
 
-   section A section
-   Completed task            :done,    des1, 2014-01-06,2014-01-08
-   Active task               :active,  des2, 2014-01-09, 3d
-   Future task               :         des3, after des2, 5d
-   Future task2              :         des4, after des3, 5d
+//    section A section
+//    Completed task            :done,    des1, 2014-01-06,2014-01-08
+//    Active task               :active,  des2, 2014-01-09, 3d
+//    Future task               :         des3, after des2, 5d
+//    Future task2              :         des4, after des3, 5d
 
-   section Critical tasks
-   Completed task in the critical line :crit, done, 2014-01-06,24h
-   Implement parser and jison          :crit, done, after des1, 2d
-   Create tests for parser             :crit, active, 3d
-   Future task in critical line        :crit, 5d
-   Create tests for renderer           :2d
-   Add to mermaid                      :1d
+//    section Critical tasks
+//    Completed task in the critical line :crit, done, 2014-01-06,24h
+//    Implement parser and jison          :crit, done, after des1, 2d
+//    Create tests for parser             :crit, active, 3d
+//    Future task in critical line        :crit, 5d
+//    Create tests for renderer           :2d
+//    Add to mermaid                      :1d
 
-   section Documentation
-   Describe gantt syntax               :active, a1, after des1, 3d
-   Add gantt diagram to demo page      :after a1  , 20h
-   Add another diagram to demo page    :doc1, after a1  , 48h
+//    section Documentation
+//    Describe gantt syntax               :active, a1, after des1, 3d
+//    Add gantt diagram to demo page      :after a1  , 20h
+//    Add another diagram to demo page    :doc1, after a1  , 48h
 
-   section Last section
-   Describe gantt syntax               :after doc1, 3d
-   Add gantt diagram to  page      :20h
-   Add another diagram to demo page    :48h
-`)
-})
+//    section Last section
+//    Describe gantt syntax               :after doc1, 3d
+//    Add gantt diagram to  page      :20h
+//    Add another diagram to demo page    :48h
+// `)
+// })
 
- 
+//  
