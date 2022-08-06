e512e45a6cf106408ff4f787425d2dec9f19f614
Renan LE CARO <renan.lecaro@gmail.com>
2022-08-06 09:53:01 +0200

    New logos, updated preview version of the plugin to open resulting picture iin new window

--- src/google.js
+++ src/google.js
@@ -1,12 +1,26 @@
 export function applyChanges(lastCorrectCode, base64, width, height) {
-  console.log(lastCorrectCode, base64);
 
-  if (!window.google) return window.open(base64);
-  window.google.script.run
-    .withFailureHandler(msg => alert(msg))
-    .withSuccessHandler(() => google.script.host.close())
-    .withUserObject(this)
-    .insertImage(lastCorrectCode, base64, width, height);
+  console.log(lastCorrectCode, base64);
+  if (window.google) {
+    window.google.script.run
+      .withFailureHandler(msg => alert(msg))
+      .withSuccessHandler(() => google.script.host.close())
+      .withUserObject(this)
+      .insertImage(lastCorrectCode, base64, width, height);
+  }else{
+      var win = window.open("");
+      win.document.body.innerHTML = `
+        <html>
+        <head><title>Redendered png preview</title></head>
+        <body>
+        <h1>Preview of the png</h1>
+        <p>You are running the addon outside of google docs, here's a preview of what gdocs would receive</p>
+        <img src="${base64}"/>
+        </body>
+      </html>
+    `
+  }
+
 }
 
 export function cancelEdit() {
